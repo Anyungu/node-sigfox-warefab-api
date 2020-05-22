dc1a9699968cbf01ae5abf208e3079ea014930ae
git1 <ouko.sherman@gmail.com>
2020-05-22 08:37:25 +0300

    tutorial

--- src/services/warefabPareser.mjs
+++ src/services/warefabPareser.mjs
@@ -2,24 +2,23 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 var moment = require('moment-timezone');
 
-export async function decodeData(payload) {
+export async function decodeData(payload, device) {
 
     try {
 
 
       
-        console.log(payload.data);
-        console.log(payload.device);
-
+        console.log(payload);
+        console.log(device);
 
         let lat_1, lng_1, latitude, longitute, temperature, humidity;
 
-        lat_1 = parseInt(payload.data.slice(0, 6), 16);
+        lat_1 = parseInt(payload.slice(0, 6), 16);
         latitude = (-1 * lat_1) / 10000; // LAT = (LAT/100000)-90
-        lng_1 = parseInt(payload.data.slice(7, 14), 16);
+        lng_1 = parseInt(payload.slice(7, 14), 16);
         longitute = lng_1 / 10000; // LON = (LON/100000)-180
-        temperature = parseInt(payload.data.slice(18, 20), 16);
-        humidity = parseInt(payload.data.slice(20, 22), 16);
+        temperature = parseInt(payload.slice(18, 20), 16);
+        humidity = parseInt(payload.slice(20, 22), 16);
 
         let payloaddecoded = {
             latitude: latitude,
@@ -28,7 +27,7 @@ export async function decodeData(payload) {
             humidity: humidity,
             payload: payload,
             time: moment().tz("Africa/Nairobi").format(),
-            device: payload.device
+            device: device
         };
 
         return { value: payloaddecoded};
