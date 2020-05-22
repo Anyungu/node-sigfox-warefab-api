1626b7b5f9251a03f74a44cc18091fab3e9d1b76
git1 <ouko.sherman@gmail.com>
2020-05-22 09:25:34 +0300

    tutorial

--- public/js/index.js
+++ public/js/index.js
@@ -12,16 +12,14 @@ function collectData () {
         }
     }).then(function (data) {
 
-        console.log(data);
-
         // This is the JSON from our response
-        document.getElementById("payload").innerHTML = data.value === undefined ? "Payload Not Available!" : data.value.payload;
-        document.getElementById("stamp").innerHTML = data.value === undefined ? "00:00 00:00" : data.value.time ;
-        document.getElementById("lat").innerHTML = data.value === undefined ? "x": data.value.latitude;
-        document.getElementById("lng").innerHTML = data.value === undefined ? "y": data.value.longitude;
-        document.getElementById("temp").innerHTML = data.value === undefined ? "room": data.value.temperature;
-        document.getElementById("hum").innerHTML =  data.value === undefined ? "normal" : data.value.humidity;
-        document.getElementById("dev").innerHTML =  data.value === undefined ? "xxxxxx" : data.value.device;
+        document.getElementById("payload").innerHTML = data.data.value === undefined ? "Payload Not Available!" : data.data.value.payload;
+        document.getElementById("stamp").innerHTML = data.data.value === undefined ? "00:00 00:00" : data.data.value.time ;
+        document.getElementById("lat").innerHTML = data.data.value === undefined ? "x": data.data.value.latitude;
+        document.getElementById("lng").innerHTML = data.data.value === undefined ? "y": data.data.value.longitude;
+        document.getElementById("temp").innerHTML = data.data.value === undefined ? "room": data.data.value.temperature;
+        document.getElementById("hum").innerHTML =  data.data.value === undefined ? "normal" : data.data.value.humidity;
+        document.getElementById("dev").innerHTML =  data.data.value === undefined ? "xxxxxx" : data.data.value.device;
     }).catch(function (err) {
         // There was an error
         console.warn('Something went wrong.', err);
